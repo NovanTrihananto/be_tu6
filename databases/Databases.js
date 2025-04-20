f57903193a3eaf48b2b25e7a42a350ebc03670cc
NovanTrihananto <152064002+NovanTrihananto@users.noreply.github.com>
2025-04-20 20:57:58 +0700

    Update Databases.js

--- databases/Databases.js
+++ databases/Databases.js
@@ -1,6 +1,6 @@
 import { Sequelize } from "sequelize";
 // Nyambungin db ke BE
-const db = new Sequelize("RECOVER_YOUR_DATA", "root", "", {
+const db = new Sequelize("notes", "root", "", {
  host: "35.225.111.238",
  dialect: "mysql",
 });
